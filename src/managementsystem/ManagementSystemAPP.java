1f03fbed06997db2e0de1ac932fa5d7d4d7e56ee
lin <771694112@qq.com>
2024-01-29 17:31:48 +0800

    Merge branch 'main' of https://github.com/SiwenyuFish/work3

--- src/managementsystem/ManagementSystemAPP.java
+++ src/managementsystem/ManagementSystemAPP.java
@@ -6,6 +6,8 @@ import managementsystem.view.ManagementSystemView;
 public class ManagementSystemAPP {
     public static void main(String[] args) {
         new ManagementSystemView().mianMenu();
+        System.out.println("================");
         System.out.println("你退出了订单管理系统");
+        System.out.println("================");
     }
 }
